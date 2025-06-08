b4354c9cb1576b4ff39f9050e226db8a68891596
Aarya Balan <aaryabalan2006@gmail.com>
2025-06-08 10:41:28 +0530

    structured the socket controller

---
+++ server/controllers/socketControllers.js
@@ -0,0 +1,51 @@
+const { setOnline, setOffline } = require('./userController');
+const socketControllers = (io) => {
+    return socket => {
+        let id = ''
+
+        socket.on('addUser', (userId) => {
+            if (!userId) return;
+            global.usersIdMapSocketId.set(userId, socket.id)
+            id = userId
+            setOnline(id)
+            global.onlineUsers.add(userId)
+        })
+
+        socket.on('sendMessage', data => {
+            if (!data?.reciever || !data?.message) return;
+
+            const socketIdOfReciever = global.usersIdMapSocketId.get(data.reciever)
+            if (socketIdOfReciever) {
+                socket.to(socketIdOfReciever).emit('recieveMessage', data.message)
+            }
+        })
+
+        socket.on('online', userId => {
+            if (userId) {
+                setOnline(userId)
+                global.onlineUsers.add(userId)
+                io.emit('online', Array.from(global.onlineUsers))
+            }
+        })
+
+        socket.on('typing', users => {
+            console.log(users, global.usersIdMapSocketId)
+            const socketIfOfWaitingUser = global.usersIdMapSocketId.get(users.waitingUser)
+            console.log(socketIfOfWaitingUser)
+            if (socketIfOfWaitingUser) {
+                socket.to(socketIfOfWaitingUser).emit('typing', users)
+            }
+        })
+
+        socket.on('disconnect', () => {
+            if (id) {
+                setOffline(id)
+                global.onlineUsers.delete(id)
+                io.emit('online', Array.from(global.onlineUsers))
+                global.usersIdMapSocketId.delete(id)
+            }
+        })
+    }
+}
+
+module.exports = socketControllers
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const app = express();
 //socket
 const socket = require('socket.io')
+const socketControllers = require('./controllers/socketControllers')
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -15,7 +16,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const messageRoutes = require('./routes/messageRoutes');
-const { setOnline, setOffline } = require('./controllers/userController');
 
 app.use('/api/auth', authRoutes);
 app.use('/users', userRoutes);
@@ -39,57 +39,4 @@ const io = socket(server, {
 global.usersIdMapSocketId = new Map()
 global.onlineUsers = new Set()
 
-io.on('connection', socket => {
-    let id = ''
-
-    socket.on('addUser', (userId) => {
-        if (!userId) return;
-        global.usersIdMapSocketId.set(userId, socket.id)
-        id = userId
-        setOnline(id)
-        global.onlineUsers.add(userId)
-    })
-
-    socket.on('sendMessage', data => {
-        if (!data?.reciever || !data?.message) return;
-
-        const socketIdOfReciever = global.usersIdMapSocketId.get(data.reciever)
-        if (socketIdOfReciever) {
-            socket.to(socketIdOfReciever).emit('recieveMessage', data.message)
-        }
-    })
-
-    socket.on('online', userId => {
-        if (userId) {
-            setOnline(userId)
-            global.onlineUsers.add(userId)
-            io.emit('online', Array.from(global.onlineUsers))
-        }
-    })
-
-    // socket.on('offline', userId => {
-    //     if (userId) {
-    //         setOffline(userId)
-    //         global.onlineUsers.delete(userId)
-    //         io.emit('offline', global.onlineUsers)
-    //     }
-    // })
-
-    socket.on('typing', users => {
-        console.log(users, global.usersIdMapSocketId)
-        const socketIfOfWaitingUser = global.usersIdMapSocketId.get(users.waitingUser)
-        console.log(socketIfOfWaitingUser)
-        if (socketIfOfWaitingUser) {
-            socket.to(socketIfOfWaitingUser).emit('typing', users)
-        }
-    })
-
-    socket.on('disconnect', () => {
-        if (id) {
-            setOffline(id)
-            global.onlineUsers.delete(id)
-            io.emit('online', Array.from(global.onlineUsers))
-            global.usersIdMapSocketId.delete(id)
-        }
-    })
-})
+io.on('connection', socketControllers(io))
